f0c5fd3d0b0af4dcbfd3535a18d016e008c21d73
Alwin <haklander@gmail.com>
2019-10-09 20:04:18 +0200

    added -w option for offline validator nodes

--- monitor_nodes.sh
+++ monitor_nodes.sh
@@ -1,8 +1,16 @@
 #/bin/bash
 
+# EXAMPLE:
+# bash monitor_nodes.sh -w			--> shows all offline validator nodes
+# bash monitor_nodes.sh string1 string2 string3	--> shows all nodes with string in their json info
+
 # user needs to specify at least 1 search term as an argument (case sensitive)
 if [ "$#" == 0 ]; then
-	echo "Please specify at least 1 search term, like part of the node display name or initialNodesPk. Exiting script."
+	echo
+	echo "Please specify at least 1 search term, like part of the node display name or initialNodesPk."
+	echo "bash monitor_nodes.sh -w	--> Show all offline validator nodes"
+	echo "Exiting script."
+	echo
 	exit
 fi
 
@@ -13,7 +21,19 @@ fi
 
 # creating the grep command, based on the arguments passed to the script
 if [ "$#" == 1 ]; then
-	grepstring="-o '\{[^\{]*'"$1"'[^\}]*\}'"
+	if [ "$1" == "-w" ]; then
+		echo
+		echo "------------ OFFLINE VALIDATOR NODES ------------"
+		echo -e "initialNodesPk\tVal?\tUp(s)\tDown(s)\tNode name"
+		curl --silent http://localhost:8080/node/heartbeatstatus | \
+		grep -o '\{[^\{]*\"isActive\":false[^\}]*\"isValidator\":true[^\}]*\}' | \
+			jq -s -c --raw-output 'sort_by(.nodeDisplayName)[] | [.hexPublicKey[0:12],.isValidator,.totalUpTimeSec,.totalDownTimeSec,.nodeDisplayName] | @tsv'
+		echo "-------------------------------------------------"
+		echo
+		exit
+	else
+		grepstring="-o '\{[^\{]*'"$1"'[^\}]*\}'"
+	fi
 fi
 if [ "$#" -ge 2 ]; then
 	grepstring="-o -e '\{[^\{]*'"$1"'[^\}]*\}'"
