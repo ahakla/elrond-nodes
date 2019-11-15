0ab167ab70cc8d66134ec6961bb4a1844559fed2
Alwin <haklander@gmail.com>
2019-11-15 22:01:20 +0100

    modified check_autorestart_nodes_tmux.sh

--- check_autorestart_nodes_tmux.sh
+++ check_autorestart_nodes_tmux.sh
@@ -22,6 +22,7 @@ begin=$(date +%s)
 keypress=''
 while [ "x$keypress" = "x" ]; do
 	echo
+	echo " Node | Sync | initNodes Pk | Typ | Node Display Name | Shard | ConP | Synch Block Nonce | Consensus Round"
 	for i in $list_node_index; do
 	        rest_api_port=$((8080+i))
 		node_status[i]="$(curl --silent http://localhost:$rest_api_port/node/status)"
@@ -29,13 +30,23 @@ while [ "x$keypress" = "x" ]; do
 	        # Check if rest-api-port is open
 	        if [[ "${RESTAPI_KEYS[i]^^}" == "YES" ]]; then
 			if [[ ! -z $(echo ${node_status[i]} | jq '.details.erd_app_version') ]]; then
-				erd_node_display_name[i]="$(echo ${node_status[i]} | jq '.details.erd_node_display_name')"
+				erd_is_syncing_str[i]="OK"
+				erd_is_syncing[i]="$(echo ${node_status[i]} | jq '.details.erd_is_syncing')"
+				if [[ $((erd_is_syncing[i])) != 0 ]]; then erd_is_syncing_str[i]="!!"; fi
+				erd_node_display_name[i]="$(echo ${node_status[i]} | jq '.details.erd_node_display_name' | tr -d '"')"
+				erd_public_key_block_sign[i]="$(echo ${node_status[i]} | jq '.details.erd_public_key_block_sign' | tr -d '"')"
 				erd_shard_id[i]="$(echo ${node_status[i]} | jq '.details.erd_shard_id')"
 				if [[ $((erd_shard_id[i]-1000000)) -gt 0 ]]; then erd_shard_id[i]="meta"; fi
+				erd_node_type[i]="$(echo ${node_status[i]} | jq '.details.erd_node_type' | tr -d '"')"
 				erd_num_connected_peers[i]="$(echo ${node_status[i]} | jq '.details.erd_num_connected_peers')"
-				printf "Node %d/%d | %-20s | Shard %-4s | %d connected peers\n" \
-					$((i+1)) $list_node_length "${erd_node_display_name[i]}" "${erd_shard_id[i]}" \
-					"${erd_num_connected_peers[i]}"
+				erd_nonce[i]="$(echo ${node_status[i]} | jq '.details.erd_nonce')"
+				erd_probable_highest_nonce[i]="$(echo ${node_status[i]} | jq '.details.erd_probable_highest_nonce')"
+				erd_synchronized_round[i]="$(echo ${node_status[i]} | jq '.details.erd_synchronized_round')"
+				erd_current_round[i]="$(echo ${node_status[i]} | jq '.details.erd_current_round')"
+				printf "%2d/%-2d |  %2s  | %-12s | %-3s | %-17s | %5s | %4d | %8d/%-8d | %8d/%-8d \n" \
+					$((i+1)) $list_node_length "${erd_is_syncing_str[i]}" "${erd_public_key_block_sign[i]:0:12}" "${erd_node_type[i]:0:3}" "${erd_node_display_name[i]:0:17}" "${erd_shard_id[i]}" \
+					"${erd_num_connected_peers[i]}" "${erd_nonce[i]}" "${erd_probable_highest_nonce[i]}" \
+					"${erd_synchronized_round[i]}" "${erd_current_round[i]}"
 			else
 				printf "Node %d/%d down!\n" \
 					$((i+1)) $list_node_length
